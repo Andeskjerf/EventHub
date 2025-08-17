c638f6ad751752c477a3dd9dd52c72d070fab636
Andeskjerf <predgd@pm.me>
2025-08-17 13:21:47 +0200

    add spring security

--- backend/src/main/java/sh/lmao/event_hub/EventHubApplication.java
+++ backend/src/main/java/sh/lmao/event_hub/EventHubApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventHubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventHubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EventHubApplication.class, args);
+    }
 
 }
