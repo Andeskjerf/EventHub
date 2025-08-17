3cfbcd587e47906d38cb1c1c8c47467bec2ddf57
Andeskjerf <predgd@pm.me>
2025-08-17 13:06:41 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# EventHub
+
+A website where you can register your participation for any upcoming events
+
+Built with Vue & Java Spring.
---
+++ backend/src/test/java/sh/lmao/event_hub/EventHubApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.lmao.event_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
