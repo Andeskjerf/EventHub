0d89f1843e65b4d359b218f6f1c2b8a1f4ea08e2
Andeskjerf <predgd@pm.me>
2025-08-18 12:27:40 +0200

    make it compile

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 A website where you can register your participation for any upcoming events
 
 Built with Vue & Java Spring.
+
+## Getting started
+
+### Setting up database
+
+```shell
+touch src/main/resources/database.db
+```
+
+### Generating keys used for JWT
+
+```shell
+openssl genpkey -algorithm RSA -out src/main/resources/key.pem
+openssl rsa -pubout -in src/main/resources/key.pem -out src/main/resources/key.pub
+```
---
+++ backend/src/main/java/sh/lmao/event_hub/converters/PublicKeyConverter.java
@@ -0,0 +1,18 @@
+package sh.lmao.event_hub.converters;
+
+import java.io.ByteArrayInputStream;
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.converter.RsaKeyConverters;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesBinding
+public class PublicKeyConverter implements Converter<String, RSAPublicKey> {
+    @Override
+    public RSAPublicKey convert(String from) {
+        return RsaKeyConverters.x509().convert(new ByteArrayInputStream(from.getBytes()));
+    }
+}
