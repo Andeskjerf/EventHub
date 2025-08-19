f7742ee21bcc3cd8745951f496d6549e3f3c8dd2
Andeskjerf <predgd@pm.me>
2025-08-19 19:10:34 +0200

    add repos

---
+++ backend/src/main/java/sh/lmao/event_hub/repositories/ParticipantRepo.java
@@ -0,0 +1,12 @@
+package sh.lmao.event_hub.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import sh.lmao.event_hub.entities.Participant;
+
+@Repository
+public interface ParticipantRepo extends JpaRepository<Participant, UUID> {
+}
