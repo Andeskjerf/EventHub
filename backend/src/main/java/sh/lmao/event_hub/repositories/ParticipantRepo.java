d3c89781331dca045ccbdd82a131989536b7b9f1
Andeskjerf <predgd@pm.me>
2025-08-19 19:42:38 +0200

    add method to get participants by activity id

--- backend/src/main/java/sh/lmao/event_hub/repositories/ParticipantRepo.java
+++ backend/src/main/java/sh/lmao/event_hub/repositories/ParticipantRepo.java
@@ -1,5 +1,6 @@
 package sh.lmao.event_hub.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,5 @@ import sh.lmao.event_hub.entities.Participant;
 
 @Repository
 public interface ParticipantRepo extends JpaRepository<Participant, UUID> {
+    List<Participant> findByActivityId(UUID activityId);
 }
