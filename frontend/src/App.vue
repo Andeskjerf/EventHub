d00d9ab68dd89bedd1b4e3a733159eab10951c97
Andeskjerf <predgd@pm.me>
2025-08-21 12:59:20 +0200

    basic pages & routing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
+  <nav id="navigation">
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/auth">Go to Auth</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+#navigation {
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+import Authentication from "@/pages/Authentication.vue";
+import Home from "@/pages/Home.vue";
+
+const routes = [
+	{ path: "/", component: Home },
+	{ path: "/auth", component: Authentication },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: routes,
+});
 
-export default router
+export default router;
