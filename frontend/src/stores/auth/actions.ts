c0a218099dd64fc03277eede6b20b1f1b80ec8b1
Andeskjerf <predgd@pm.me>
2025-08-22 12:33:27 +0200

    ...do remember to update store with auth state

--- frontend/src/stores/auth/actions.ts
+++ frontend/src/stores/auth/actions.ts
@@ -1,8 +1,9 @@
-import { getUsername } from "@/services/storage";
+import { getUsername, isAuthenticated } from "@/services/storage";
 import { userModule } from "./module";
 
 const updateAuthState = () => {
 	userModule.state.username = getUsername();
+	userModule.state.isAuthenticated = isAuthenticated();
 };
 
 export default {
