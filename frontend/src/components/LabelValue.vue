3156982ef3f53e0983ca27700dfa1ebcd12223fd
Andeskjerf <predgd@pm.me>
2025-08-24 23:55:26 +0200

    styling for component

--- frontend/src/components/LabelValue.vue
+++ frontend/src/components/LabelValue.vue
@@ -8,10 +8,15 @@ const { label, value } = defineProps<{
 </script>
 
 <template>
-  <div id="container">
+  <div>
     <div id="label">{{ label }}</div>
     <div id="value">{{ value }}</div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#label {
+  color: gray;
+  font-size: 12px;
+}
+</style>
