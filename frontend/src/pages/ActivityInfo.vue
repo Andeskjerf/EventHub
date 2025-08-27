58cb5b88a45cb9d0c879974066e12a8327f2b9a2
Andeskjerf <predgd@pm.me>
2025-08-27 08:46:25 +0200

    give default value instead of hiding

--- frontend/src/pages/ActivityInfo.vue
+++ frontend/src/pages/ActivityInfo.vue
@@ -140,7 +140,7 @@ function setId(instanceId: string) {
       <LabelValue label="Tid" :value="activityUtils.formatEventTime(activity.eventDate)" />
       <LabelValue label="Dag" :value="activityUtils.formatEventDate(activity.eventDate)" />
       <LabelValue label="Sted" :value="activity.location" />
-      <LabelValue v-if="activity.meetLocation.length > 0" label="Oppmøte" :value="activity.meetLocation" />
+      <LabelValue label="Oppmøte" :value="activity.meetLocation || 'Ikke angitt'" />
     </div>
 
     <div class="description">
