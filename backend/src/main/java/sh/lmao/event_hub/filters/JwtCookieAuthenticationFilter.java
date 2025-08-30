1a0efe22ebadb339574e3911f3fb0f496dc4ba58
Andeskjerf <predgd@pm.me>
2025-08-30 11:36:32 +0200

    handle invalid jwt tokens

--- backend/src/main/java/sh/lmao/event_hub/filters/JwtCookieAuthenticationFilter.java
+++ backend/src/main/java/sh/lmao/event_hub/filters/JwtCookieAuthenticationFilter.java
@@ -2,6 +2,7 @@ package sh.lmao.event_hub.filters;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,16 +12,20 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletRequestWrapper;
 import jakarta.servlet.http.HttpServletResponse;
+import sh.lmao.event_hub.security.JWTUtil;
 
 @Component
 public class JwtCookieAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTUtil jwtUtil;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
         if (request.getHeader("Authorization") == null) {
             String jwt = extractJwtFromCookie(request);
-            if (jwt != null) {
+            if (jwtUtil.isJwtTokenValid(jwt) && jwt != null) {
                 HttpServletRequestWrapper wrapper = new HttpServletRequestWrapper(request) {
                     @Override
                     public String getHeader(String name) {
--- backend/src/main/java/sh/lmao/event_hub/security/JWTUtil.java
+++ backend/src/main/java/sh/lmao/event_hub/security/JWTUtil.java
@@ -1,28 +1,39 @@
 package sh.lmao.event_hub.security;
 
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Instant;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.jwt.BadJwtException;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.stereotype.Component;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 @Component
 public class JWTUtil {
 
     private static final Logger logger = LoggerFactory.getLogger(JWTUtil.class);
 
-    @Autowired
-    JwtEncoder encoder;
+    @Value("${jwt.public.key}")
+    RSAPublicKey key;
 
-    @Autowired
-    JwtDecoder decoder;
+    @Value("${jwt.private.key}")
+    RSAPrivateKey priv;
 
     public static long tokenExpiration = 36000L;
 
@@ -38,28 +49,35 @@ public class JWTUtil {
                 .subject(username)
                 .claim("scope", "app")
                 .build();
-        return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+        return jwtEncoder().encode(JwtEncoderParameters.from(claims)).getTokenValue();
 
     }
 
-    public String generateToken(Authentication authentication) {
-        Instant now = Instant.now();
-        // String scope = authentication.getAuthorities().stream()
-        // .map(GrantedAuthority::getAuthority)
-        // .collect(Collectors.joining(" "));
-        JwtClaimsSet claims = JwtClaimsSet.builder()
-                .issuer("self")
-                .issuedAt(now)
-                .expiresAt(now.plusSeconds(tokenExpiration))
-                .subject(authentication.getName())
-                .claim("scope", "app")
-                .build();
-        return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
-
+    public boolean isJwtTokenValid(String token) {
+        // we check the validity by the token by simply trying to decode it
+        // it'll fail with a signature error if it's invalid
+        try {
+            jwtDecoder().decode(token);
+            return true;
+        } catch (BadJwtException e) {
+            logger.warn("failed to verify JWT token signature. this is most likely due to new RSA keys.");
+            return false;
+        } catch (Exception e) {
+            logger.error("got an unknown error while checking JWT token validity!");
+            logger.error(e.toString());
+            return false;
+        }
     }
 
-    public void isJwtTokenValid(String token) {
-        Jwt jwt = decoder.decode(token);
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(this.key).build();
+    }
 
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(this.key).privateKey(this.priv).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 }
